b4ab108e851865ef2fc2f0d830095feb8610b2a8
slevy2020 <slevy@chadwickschool.org>
2019-02-15 11:47:31 -0800

    feat: add content from lesson 23

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+  //public variables
+  public Vector3 axis; //which axis to spin on - mark the axis with a 1
+  public float speed = 1.0f; //speed multiplier
+
+  void Update () {
+    //rotate the object
+    float amount = speed * Time.deltaTime; //mult * time since last Update
+    transform.Rotate(axis.x * amount, axis.y * amount, axis.z * amount);
+  }
+}
