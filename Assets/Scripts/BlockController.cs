5da2cb4bd5224537b9440445d19a08674de51b6a
slevy2020 <slevy@chadwickschool.org>
2019-02-22 10:40:57 -0800

    feat: add some content from lesson 28
    class is over, saving work

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+  //public variables
+  public Vector3 speed; //how fast and on what axis to move on, 0 means don't move
+
+  //private variables
+  private int direction; //what direction to move along axis
+
+  void Start () {
+    direction = 1; //start in the + direction
+  }
+
+  void Update () {
+    //move the block
+    transform.position += speed * direction * Time.deltaTime;
+  }
+
+  void OnCollisionEnter () {
+    Debug.Log("Block hit");
+    //flip the direction
+    direction *= -1;
+  }
+}
