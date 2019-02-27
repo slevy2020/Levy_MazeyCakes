3cd8452cf406c550fead27a9ee876b4df637fdd0
slevy2020 <slevy@chadwickschool.org>
2019-02-26 23:49:22 -0800

    feat: add most of content from lesson 32
    it's late, i'm tired, i'm just gonna work on this in class tomorrow

---
+++ Assets/Scripts/PersistentData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentData : MonoBehaviour {
+
+  //private variables
+  private bool win;
+  private int time;
+
+  void Start () {
+    Reset();
+    //make this object persistent
+    DontDestroyOnLoad(transform.gameObject);
+  }
+
+  void Reset () {
+    win = false;
+    time = 0;
+  }
+
+  public void SetWin (bool state) {
+    //record provided win state
+    win = state;
+  }
+}
